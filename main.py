f10653c486e97d2f96a3f8753d849f28a55ab33b
PogodinSergey <cameravolodarskogo@gmail.com>
2025-03-08 20:14:15 +0300

    Task 7

--- main.py
+++ main.py
@@ -229,5 +229,36 @@ def astronaut_selection():
         return "Форма отправлена"
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    result = [nickname, str(level), str(rating)]
+
+    return """<!DOCTYPE html>
+        <html lang="ru">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+            <title>Результаты</title>
+        </head>
+        <body>
+            <h1>Результаты отбора</h1>
+            <h3>Претендента на участие в миссии {}: </h3>
+            <div class="alert alert-success" role="alert">
+                <h3>Поздравляем! Ваш рейтинг после {} этапа отбора</h3>
+            </div>
+            <div class="alert alert-none" role="alert">
+                <h3>Составляет {}!</h3>
+            </div>
+            <div class="alert alert-warning" role="alert">
+                <h3>Желаем удачи!</h3>
+            </div>
+        </body>
+        </html>""".format(*result)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
