415524c3fdf07f655fb7c739414f2a2b50837f02
PogodinSergey <cameravolodarskogo@gmail.com>
2025-03-11 20:23:50 +0300

    Task 10

--- main.py
+++ main.py
@@ -1,59 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet_name>')
-def planet(planet_name="Сатурн"):
-    name = planet_name.lower()
-    if name == "марс":
-        result = ["Эта планета близка к Земле",
-                  "На ней много необходимых ресурсов",
-                  "На ней есть вода и атмосфера",
-                  "На ней есть небольшое магнитное поле",
-                  "Наконец, она просто красива!"]
-    elif name == "венера":
-        result = ["Эта планета далеко от Земле",
-                  "На ней нет необходимых ресурсов",
-                  "На ней нет воды и атмосферы",
-                  "На ней есть большое магнитное поле",
-                  "Она некрасива!"]
-    elif name == "сатурн":
-        result = ["Эта планета на среднем расстоянии от Земли",
-                  "На ней есть нужные ресурсы",
-                  "Есть немного воды и воздуха",
-                  "Среднее магнитное поле",
-                  "Она красивая!"]
-    return """<!DOCTYPE html>
-    <html lang="ru">
-    <head>
-        <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                   <link rel="stylesheet"
-                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                   crossorigin="anonymous">
-        <title>реклама</title>
-    </head>
-    <body>
-    <h1>Моё предложение: {}</h1>
-    <h2>{}</h2>
-    <div class="alert alert-primary" role="alert">
-        <h3>{}</h3>
-    </div>
-    <div class="alert alert-info" role="alert">
-        <h3>{}</h3>
-    </div>
-    <div class="alert alert-success" role="alert">
-        <h3>{}</h3>
-    </div>
-    <div class="alert alert-warning" role="alert">
-        <h3>{}</h3>
-    </div>
-    </body>
-    </html>""".format(planet_name, *result)
+@app.route('/<title>')
+def index(title):
+    return render_template("base.html", title=title)
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions = ["инженер-строитель", "пилот",
+                   "строитель", "экзобиолог",
+                   "врач", "штурман"]
+    return render_template("list_prof.html", list_type=list_type, professions=professions)
 
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
-    # http://127.0.0.1:8080/
